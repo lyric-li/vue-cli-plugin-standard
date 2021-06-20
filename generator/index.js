e1845c390362bbb6a356933c7c62c3290ea3775c
lyric <1353758518@qq.com>
2021-06-20 17:24:10 +0800

    feat: 新增基础配置文件

--- generator/index.js
+++ generator/index.js
@@ -2,7 +2,9 @@ module.exports = (api, options) => {
   api.extendPackage({
     name: options.name,
     description: options.description,
+    browserslist: undefined,
   });
+
   // 创建新模板
-  // api.render("./template");
+  api.render("./template");
 };
