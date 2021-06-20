0ad10b9bb0b3326c5b2fc316c3ace5da02777029
lyric <1353758518@qq.com>
2021-06-20 17:17:01 +0800

    feat: init package name & description

--- generator/index.js
+++ generator/index.js
@@ -1,4 +1,8 @@
 module.exports = (api, options) => {
+  api.extendPackage({
+    name: options.name,
+    description: options.description,
+  });
   // 创建新模板
-  api.render("./template");
+  // api.render("./template");
 };
--- prompts.js
+++ prompts.js
@@ -1,6 +1,21 @@
 // 将 `package.json` 作为参数传入函数
 module.exports = (pkg) => {
-  const prompts = [];
+  const prompts = [
+    {
+      type: "input",
+      name: "name",
+      required: true,
+      message: "Your project name",
+      default: pkg.name,
+    },
+    {
+      type: "input",
+      name: "description",
+      required: false,
+      message: "Project description",
+      default: pkg.description || "",
+    },
+  ];
 
   return prompts;
 };
