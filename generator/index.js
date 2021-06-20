7c92d2bea95921e16d1c14fa0781ad12b516c560
lyric <1353758518@qq.com>
2021-06-20 18:52:46 +0800

    feat: add eslint & stylint tool

--- generator/index.js
+++ generator/index.js
@@ -3,8 +3,28 @@ module.exports = (api, options) => {
     name: options.name,
     description: options.description,
     browserslist: undefined,
+    eslintConfig: undefined,
+    devDependencies: {
+      "@vue/eslint-config-airbnb": "^5.0.2",
+      "stylelint": "^13.13.1",
+      "stylelint-config-standard": "^22.0.0",
+      "stylelint-order": "^4.1.0",
+    },
   });
 
   // 创建新模板
   api.render("./template");
 };
+
+module.exports.hooks = (api) => {
+  api.afterInvoke(() => {
+    const cp = require("child_process");
+    const cpe = cp.exec("yarn && npx vue-cli-service lint --fix");
+    cpe.stdout.on("data", function(data) {
+      console.log(data);
+    });
+    cpe.stderr.on("data", function(data) {
+      console.error(data);
+    });
+  })
+}
---
+++ generator/template/_eslintrc.js
@@ -0,0 +1,70 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/strongly-recommended",
+    "@vue/airbnb",
+  ],
+  parserOptions: {
+    parser: "babel-eslint",
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "linebreak-style": "off",
+    "no-shadow": "off",
+    "no-param-reassign": "off",
+    "quotes": ["error", "double"],
+    "semi": ["error", "always"],
+    "vue/order-in-components": ["error", {
+      "order": [
+        "el",
+        "name",
+        "parent",
+        "functional",
+        ["delimiters", "comments"],
+        ["components", "directives", "filters"],
+        "extends",
+        "mixins",
+        "inheritAttrs",
+        "model",
+        ["props", "propsData"],
+        "data",
+        "computed",
+        "watch",
+        "LIFECYCLE_HOOKS",
+        "methods",
+        ["template", "render"],
+        "renderError"
+      ]
+    }],
+    "vue/attributes-order": ["error", {
+      "order": [
+        "DEFINITION",
+        "LIST_RENDERING",
+        "CONDITIONALS",
+        "RENDER_MODIFIERS",
+        "GLOBAL",
+        "UNIQUE",
+        "TWO_WAY_BINDING",
+        "OTHER_DIRECTIVES",
+        "OTHER_ATTR",
+        "EVENTS",
+        "CONTENT"
+      ]
+    }],
+  },
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+      ],
+      env: {
+        mocha: true,
+      },
+    },
+  ],
+};
