e3a82b9c1425b73c4672a05ba73781ecf92f2762
lyric <1353758518@qq.com>
2021-07-22 10:35:11 +0800

    refactor: 优化 less 全局变量引入方式

---
+++ webpack/modules/less.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const AbstractWebpack = require("../abstract-webpack");
+
+class Less extends AbstractWebpack {
+  chainWebpack(config) {
+    super.chainWebpack(config);
+
+    [
+      ...config.module.rule("less").oneOfs.values(),
+    ].forEach((rule) => {
+      rule.use("less-loader").options({
+        globalVars: {
+          // 引用 theme.less 文件（文件路径为绝对路径）
+          hack: `true; @import "${path.resolve(process.cwd(), "src/assets/styles/theme.less")}";`,
+        },
+        modifyVars: {
+          // 通过 modify.less 文件覆盖（文件路径为绝对路径）
+          hack: `true; @import "${path.resolve(process.cwd(), "src/assets/styles/modify.less")}";`,
+        },
+      });
+    });
+  }
+}
+
+module.exports = Less;
