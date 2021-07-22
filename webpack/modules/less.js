2aa62eca0b8cc42e4e51287d1908bdbb92628917
lyric <1353758518@qq.com>
2021-07-22 14:09:45 +0800

    fix: 修复 less 配置异常问题

--- webpack/modules/less.js
+++ webpack/modules/less.js
@@ -9,14 +9,16 @@ class Less extends AbstractWebpack {
       ...config.module.rule("less").oneOfs.values(),
     ].forEach((rule) => {
       rule.use("less-loader").options({
-        globalVars: {
-          // 引用 theme.less 文件（文件路径为绝对路径）
-          hack: `true; @import "${path.resolve(process.cwd(), "src/assets/styles/theme.less")}";`,
-        },
-        modifyVars: {
-          // 通过 modify.less 文件覆盖（文件路径为绝对路径）
-          hack: `true; @import "${path.resolve(process.cwd(), "src/assets/styles/modify.less")}";`,
-        },
+        lessOptions: {
+          globalVars: {
+            // 引用 theme.less 文件（文件路径为绝对路径）
+            hack: `true; @import "${path.resolve(process.cwd(), "src/assets/styles/theme.less")}";`,
+          },
+          modifyVars: {
+            // 通过 modify.less 文件覆盖（文件路径为绝对路径）
+            hack: `true; @import "${path.resolve(process.cwd(), "src/assets/styles/modify.less")}";`,
+          },
+        }
       });
     });
   }
